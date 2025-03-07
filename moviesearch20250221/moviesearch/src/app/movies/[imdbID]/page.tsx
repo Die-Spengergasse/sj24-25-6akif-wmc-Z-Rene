1c5a226a949f61824015c53a63c2f02dfcf81a86
Rene Zver <zve230612@spengergasse.at>
2025-03-07 13:09:47 +0100

    exercise/01_movie_details solved

--- moviesearch20250221/moviesearch/src/app/movies/[imdbID]/page.tsx
+++ moviesearch20250221/moviesearch/src/app/movies/[imdbID]/page.tsx
@@ -1,8 +1,10 @@
-import MovieDetailView from "../../components/MovieDetailView"; 
+import MovieDetailView from "../../components/MovieDetailView";
 import { MovieDetail } from "../../types/MovieDetail";
 
 async function fetchMovieDetails(imdbID: string): Promise<MovieDetail> {
-  const res = await fetch(`https://www.omdbapi.com/?apikey=cd2aa4ca&i=${imdbID}&plot=full`);
+  const res = await fetch(
+    `https://www.omdbapi.com/?apikey=cd2aa4ca&i=${imdbID}&plot=full`
+  );
 
   if (!res.ok) {
     throw new Error("Fehler beim Laden der Filmdetails");
@@ -11,12 +13,25 @@ async function fetchMovieDetails(imdbID: string): Promise<MovieDetail> {
   return res.json();
 }
 
-export default async function MoviePage({ params }: { params: { imdbID: string } }) {
+export default async function MoviePage({
+  params,
+}: {
+  params: { imdbID: string };
+}) {
   if (!params || !params.imdbID) {
     return <div>Fehler: Keine IMDb-ID gefunden.</div>;
   }
 
-  const movie = await fetchMovieDetails(params.imdbID);
-
-  return <MovieDetailView movie={movie} />;
+  try {
+    const movie = await fetchMovieDetails(params.imdbID);
+    return <MovieDetailView movie={movie} />;
+  } catch (error) {
+    return (
+      <div>
+        Fehler beim Laden der Filmdetails:{" "}
+        {error instanceof Error ? error.message : "Unbekannter Fehler"}
+      </div>
+    );
+  }
 }
+
